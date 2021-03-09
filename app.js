7910323bcdc846c3ecf7c4920acda051a5f806f5
mattlaw <matt@piximo.me>
2021-03-09 11:11:48 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+var cors = require('cors')
+
+const filePath = path.join(__dirname, 'story', 'text.txt');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.get('/story', (req, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to open file.' });
+    }
+    res.status(200).json({ story: data.toString() });
+  });
+});
+
+app.post('/story', (req, res) => {
+  const newText = req.body.text;
+  if (newText.trim().length === 0) {
+    return res.status(422).json({ message: 'Text must not be empty!' });
+  }
+  fs.appendFile(filePath, newText + '\n', (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Storing the text failed.' });
+    }
+    res.status(201).json({ message: 'Text was stored!' });
+  });
+});
+
+app.listen(3000);
